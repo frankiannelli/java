9790a9713f3632d018cdb34bcba1fd8b39984c37
frankiannelli <frank.iannelli@outlook.com>
2020-10-13 15:19:54 +1100

    add getDaysDifferenceTest

---
+++ tests/library/entities/CalendarTest.java
@@ -0,0 +1,22 @@
+package library.entities;
+
+import org.junit.jupiter.api.Test;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CalendarTest {
+
+    @Test
+    void getDaysDifferenceTest() throws ParseException {
+        ICalendar calendar = Calendar.getInstance();
+        Date sdf = new SimpleDateFormat("yyyy-MM-dd").parse("2008-01-02");
+        calendar.setDate(sdf);
+        Date dueDate = new SimpleDateFormat("yyyy-MM-dd").parse("2008-01-01");
+        long daysOverDue = calendar.getDaysDifference(dueDate);
+        assertEquals(daysOverDue, 1);
+    }
+}
